b764e34a7353538b2becbd7bdf04fee208fc269e
Ulan Kurmanbekov <ulankurmanbekov83@gmail.com>
2024-12-05 16:01:58 +0600

    change homepage

--- orders/urls.py
+++ orders/urls.py
@@ -3,6 +3,6 @@ from orders.views import OrderView
 
 
 urlpatterns = [
-    path('', OrderView.as_view({'get': 'list', 'post': 'create'})),
-    path('<int:pk>/', OrderView.as_view({'get': 'retrieve', 'patch': 'partial_update'})),
+    path('orders/', OrderView.as_view({'get': 'list', 'post': 'create'})),
+    path('orders/<int:pk>/', OrderView.as_view({'get': 'retrieve', 'patch': 'partial_update'})),
 ]
